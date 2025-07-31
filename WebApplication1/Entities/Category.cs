f58d8f3e81c6e06b13a255d697ee660f7ea3f8d2
ahmetvarol <dev.ahmetvarol@gmail.com>
2025-07-31 21:24:55 +0300

    Critical bug fixed

--- WebApplication1/Entities/Category.cs
+++
@@ -1,7 +0,0 @@
-using WebApplication1.Entities;
-using WebApplication1.SystemEnums;
-
-public class Category : BaseEntity
-{
-    public TestEnum EnumTestVal { get; set; }
-}
--- WebApplication1/SystemEnums/TestEnum.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication1.SystemEnums;
-
-public enum TestEnum
-{
-    Value1,
-    Value2,
-    Value3
-}
